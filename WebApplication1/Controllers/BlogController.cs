fe30b0ed84109123f05214e6e47a1100084f2d4b
timur <timurimzaliev60@gmail.com>
2020-09-24 00:39:33 +0400

    Добавьте файлы проекта.

---
+++ WebApplication1/Controllers/BlogController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    /// <summary>
+    /// Контроллер для работы с блогом
+    /// </summary>
+    public class BlogController : Controller
+    {
+        /// <summary>
+        /// Возвращение представления Blog возвратом Index.cshtml
+        /// </summary>
+        /// <returns>View</returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    /// <summary>
+    /// Контроллер для работы с Home
+    /// </summary>
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        /// <summary>
+        /// Конструктор класса <see cref="HomeController">
+        /// </summary>
+        /// <param name="logger"></param>
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Возвращение представления Home возвратом Index.cshtml
+        /// </summary>
+        /// <returns>View</returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Возвращение представления Home возвратом Privacy.cshtml
+        /// </summary>
+        /// <returns>View</returns>
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Возвращение корректного отображения на случай ошибки
+        /// </summary>
+        /// <returns>View</returns>
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
