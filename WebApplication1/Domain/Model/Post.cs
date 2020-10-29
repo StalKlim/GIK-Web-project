c458e052f28d88630b37a228f6f5a727d21d9046
Григорьев Александр <foxamurai@gmail.com>
2020-10-29 15:03:24 +0400

    post api

--- WebApplication1/Domain/Model/Post.cs
+++ WebApplication1/Domain/Model/Post.cs
@@ -8,10 +8,6 @@ namespace WebApplication1.Domain.Model
     /// </summary>
     public class Post : Entity
     {
-        /// <summary>
-        /// Пользователь, который создал сущность
-        /// </summary>
-        public Client Owner { get; set; }
 
         /// <summary>
         /// Дата и время создания поста
