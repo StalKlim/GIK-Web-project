70ef67b2fce2f17cb4c163be3ff6a641a71cf6e4
timur <timurimzaliev60@gmail.com>
2020-11-07 14:34:52 +0400

    убрал owner из post

--- WebApplication1/Domain/Model/Post.cs
+++ WebApplication1/Domain/Model/Post.cs
@@ -9,11 +9,6 @@ namespace WebApplication1.Domain.Model
     public class Post : Entity
     {
         /// <summary>
-        /// Пользователь, который создал сущность
-        /// </summary>
-        public Client Owner { get; set; }
-
-        /// <summary>
         /// Дата и время создания поста
         /// </summary>
         public DateTime Created { get; set; }
