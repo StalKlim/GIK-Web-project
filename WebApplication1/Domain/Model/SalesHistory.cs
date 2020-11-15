8c626b022464b4e2afb0ab9cff959fd72c25c7cc
Foxamurai <Foxamurai@gmail.com>
2020-11-15 16:55:53 +0400

    vlf

---
+++ WebApplication1/Domain/Model/SalesHistory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Domain.Model
+{
+    public class SalesHistory
+    {
+        /// <summary>
+        /// Пользователь
+        /// </summary>
+        public Client Client { get; set; }
+
+        /// <summary>
+        /// Товар
+        /// </summary>
+        public Product Product { get; set; }
+
+        /// <summary>
+        /// Статус продажи
+        /// </summary>
+        public bool IsSold { get; set; }
+
+        /// <summary>
+        /// Дата продажи
+        /// </summary>
+        public DateTime SaleDate { get; set; }
+    }
+}
