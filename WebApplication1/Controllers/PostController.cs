2d26321f098938a09cba1c7b0da0ce3934769a25
Григорьев Александр <foxamurai@gmail.com>
2020-11-21 14:55:58 +0400

    Удалил блог, Добавил страницу новостей и конкретной новости

--- WebApplication1/Controllers/PostController.cs
+++ WebApplication1/Controllers/PostController.cs
@@ -1,25 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 using WebApplication1.Domain.DB;
+using WebApplication1.Domain.Model;
+using WebApplication1.Services;
 using WebApplication1.ViewModels.Post;
 
 namespace WebApplication1.Controllers
 {
+    /// <summary>
+    /// Контроллер для работы со страницами новостей
+    /// </summary>
     public class PostController : Controller
     {
         private readonly MarketDbContext _marketDbContext;
+        private PostServices _postServices;
 
         public PostController(MarketDbContext marketDbContext)
         {
             _marketDbContext = marketDbContext ?? throw new ArgumentNullException(nameof(marketDbContext));
+            _postServices = new PostServices(_marketDbContext);
         }
 
+        /// <summary>
+        /// Получить список новостей
+        /// </summary>
+        /// <returns>Возвращает страницу со списком новостей</returns>
+        [HttpGet]
         public IActionResult Index()
         {
             var posts = _marketDbContext.Posts
                 .Select(x => new PostCardViewModel
                 {
+                    Id = x.Id,
                     Created = x.Created,
                     FileId = x.FileId,
                     Title = x.Title,
@@ -29,5 +43,30 @@ namespace WebApplication1.Controllers
 
             return View(posts);
         }
+
+        /// <summary>
+        /// Перейти на страницу конкретной новости
+        /// </summary>
+        /// <param name="id">Идентификатор новости</param>
+        /// <returns>Возвращает страницу конкретной новости</returns>
+        [HttpGet]
+        public async Task<ActionResult<Post>> Details(long id)
+        {
+            var post = await _postServices.GetPostsByIdAsync(id);
+
+            var model = new PostDetailViewModel
+            {
+                Created = post.Created,
+                Title = post.Title,
+                Description = post.Description,
+                FileId = post.FileId,
+                Data = post.Data
+                
+            };
+            
+            
+            return View(model);
+        }
+
     }
 }
--- WebApplication1/ViewModels/Post/PostDetailViewModel.cs
+++ WebApplication1/ViewModels/Post/PostDetailViewModel.cs
@@ -7,6 +7,29 @@ namespace WebApplication1.ViewModels.Post
 {
     public class PostDetailViewModel
     {
-         
+        /// <summary>
+        /// Дата и время создания поста
+        /// </summary>
+        public DateTime Created { get; set; }
+
+        /// <summary>
+        /// Заголовок поста
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Описание поста
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Изображение к посту
+        /// </summary>
+        public string FileId { get; set; }
+
+        /// <summary>
+        /// Данные поста
+        /// </summary>
+        public string Data { get; set; }
     }
 }
