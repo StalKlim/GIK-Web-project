932372cf2428b8f93b45aa492ca2ab5fe2f442f2
timur <timurimzaliev60@gmail.com>
2020-11-27 12:55:03 +0400

    фиксация фиксаций

--- WebApplication1/Services/PostServices.cs
+++ WebApplication1/Services/PostServices.cs
@@ -1,27 +1,30 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using WebApplication1.Domain.DB;
 using WebApplication1.Domain.Model;
 using WebApplication1.DTO;
+using WebApplication1.ViewModels.Post;
 
 namespace WebApplication1.Services
 {
+    /// <summary>
+    /// Сервис для взаимодействия с постами
+    /// </summary>
     public class PostServices
     {
         private readonly MarketDbContext _marketDbContext;
 
-        /// <summary>
-        /// 
-        /// </summary>
         public PostServices(MarketDbContext marketDbContext)
         {
             _marketDbContext = marketDbContext ?? throw new ArgumentNullException(nameof(marketDbContext));
         }
 
+        /// <summary>
+        /// Получение поста по id
+        /// </summary>
+        /// <param name="id">Id поста</param>
+        /// <returns>Пост по его Id или первый пост, если не найден нужный</returns>
         public async Task<Post> GetPostsByIdAsync(long id)
         {
             return await _marketDbContext.Posts.FirstOrDefaultAsync(x => x.Id == id);
@@ -29,8 +32,18 @@ namespace WebApplication1.Services
 
         public void UpdatePost(Post post, PostDTO data)
         {
-            
+
         }
 
+        /// <summary>
+        /// Обновление поста
+        /// </summary>
+        /// <param name="id">Id поста</param>
+        /// <param name="model">Данные</param>
+        /// <returns>Пост по его Id или первый пост, если не найден нужный</returns>
+        public async Task<Post> UpdatePostMVC(long id, PostDetailViewModel model)
+        {
+            return await _marketDbContext.Posts.FirstOrDefaultAsync(x => x.Id == id);
+        }
     }
 }
---
+++ WebApplication1/ViewModels/Post/NewPostViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.ViewModels.Post
+{
+    /// <summary>
+    /// Добавление нового поста
+    /// </summary>
+    public class NewPostViewModel
+    {
+        /// <summary>
+        /// Заголовок поста
+        /// </summary>
+        [Required]
+        [Display(Name = "Заголовок")]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Изображение к посту
+        /// </summary>
+        [Display(Name = "Изображение")]
+        public string FileId { get; set; }
+
+        /// <summary>
+        /// Описание к посту
+        /// </summary>
+        [Required]
+        [Display(Name = "Описание")]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Основная новость
+        /// </summary>
+        [Required]
+        [Display(Name = "Новость")]
+        public string Data { get; set; }
+    }
+}
